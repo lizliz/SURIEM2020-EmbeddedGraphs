ff8210e5d2e7b98c20f89aa53fc1a29c2b4657cf
Candace Todd <clt5441@psu.edu>
2020-06-18 18:40:42 -0400

    osm files now also return a position dictionary

--- lib/osm2nx.py
+++ lib/osm2nx.py
@@ -143,5 +143,5 @@ def read_osm(filePath, draw = True):
         pos = nx.spring_layout(G,pos=fixed_positions, fixed = fixed_nodes)
         nx.draw_networkx(G,pos, with_labels = False, **options)
     
-    # Returns the graph object
-    return G
+    # Returns the graph object and position dictionary
+    return [G, fixed_positions]
