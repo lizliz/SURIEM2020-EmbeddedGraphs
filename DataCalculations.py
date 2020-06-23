9bf0e6e37c1ef00bbf15413f87b430879983ce02
Candace Todd <clt5441@psu.edu>
2020-06-23 13:26:18 -0400

    added bootstrapping method to DataCalculations.py

--- DataCalculations.py
+++ DataCalculations.py
@@ -7,6 +7,9 @@ Created on Mon Jun 22 19:15:49 2020
 from Merge import merge_tree, calc_values_height_reorient
 import Compare
 import math
+import random
+import statistics
+from scipy import stats
 
 def distance_data(G1, pos1, G2, pos2, frames=720, rotate_both=True, accuracy=0.0001):
     data=[]
@@ -37,3 +40,43 @@ def average_distance(G1, pos1, G2, pos2, frames=360, rotate_both=True, accuracy=
     heights = [x[1] for x in data]
     
     return sum(heights)/frames
+
+# data: data returned by the distance_data() funtion
+# samples: size of the sampling distribution you want. 3,000 is usually pretty good
+# alpha: significance level of the confidence interval, complement of confidence level
+#interpretation: whether or not you want to print a generic interpretation of the confidence interval
+def bootstrap(data, samples = 3000, alpha = 0.05, interpretation = False):
+    n = len(data) # size of sample
+    originalSample = [data[i][1] for i in range(n)] # list containing only the data points
+    point_estimate = statistics.mean(originalSample) # get mean of our sample as the estimate we center our interval around
+    bootstrap_distribution = []
+    confidence_level = 1-alpha
+    
+    for j in range(samples):
+        current_sample = random.choices(originalSample, n) # Sample from the original sample with replacement
+        current_mean = statistics.mean(current_sample) # Find mean of this bootstrap sample
+        bootstrap_distribution.append(current_mean) # Add bootstrap mean to the distribution of means
+    
+    # Standard error is the standard deviation of the sampling distribution
+    standard_error = statistics.stdev(bootstrap_distribution)
+    # using a t distribution, find the multiplier for our confidence interval
+    t_multiplier = stats.t.ppf(confidence_level,n)
+    #construct margin of error
+    moe = t_multiplier*standard_error
+    
+    # Create a dictionary so you can access the info you want or print it out nicely
+    report = {"Estimated Mean":point_estimate,
+              "Margin of Error": moe,
+              "Confidence Level": confidence_level,
+              "Confidence Interval": [point_estimate-moe, point_estimate+moe]} 
+    
+    # Prints a generic interpretation of confidence intervals
+    if interpretation == True:
+        print("We are ", confidence_level*100, "% confident that the interval from ", point_estimate-moe, "to ", point_estimate+moe, "captures the true population mean.")
+        print("If we were to construct a large number of these confidence intervals, we would expect about ", confidence_level*100, "% of them to capture the true population mean.")
+    
+    # returns the report dictionary
+    return report
+    
+    
+    
