7dfa02031056336e6afcffd4e6b5d947b194547a
Candace Todd <clt5441@psu.edu>
2020-06-24 22:46:07 -0400

    Started/Stopped confusion matrix code but left it in the Classify.py script just in case

--- Classify.py
+++ Classify.py
@@ -6,6 +6,7 @@ import scipy.cluster.hierarchy as shc
 import scipy.spatial.distance as ssd
 import pandas as pd
 import numpy as np
+#from sympy import Matrix, pprint# old code from confusion matrix days
 from sklearn.preprocessing import normalize
 import matplotlib.pyplot as plt
 from DataCalculations import average_distance
@@ -18,12 +19,49 @@ import Visualization as v
 
 #Input list should be a list of pairs of graphs & positions
 def draw_dendro(input_list, frames=180, labels=None, thresh=None):
-    count = len(input_list)
+    # count = len(input_list)
+    data = get_data(input_list, frames)    
+    # data = np.zeros(shape=(count,count))
+    # for i in range(count):
+    #     for j in range(i, count):
+    #         print("(",i,",",j,")")
+    #         if(i==j):
+    #             val=0
+    #         else:
+    #             G1 = input_list[i][0]
+    #             pos1 = input_list[i][1]
+    #             G2 = input_list[j][0]
+    #             pos2 = input_list[j][1]
+    #             val=average_distance(G1, pos1, G2, pos2, frames=frames)
+            
+    #         data[i,j] = val
+    #         data[j,i] = val
     
+    
+    dendrogram(data, labels=labels, thresh=thresh)
+    return data
+            
+
+def dendrogram(data, labels=None, thresh=None):
+    plt.figure(figsize=(10, 7))  
+    plt.title("Dendrograms")
+    #data = ssd.squareform(data)
+    lkg = shc.linkage(data, method='single')
+    dend = shc.dendrogram(lkg, labels=labels, color_threshold=thresh)
+    if(thresh != None):
+        plt.axhline(y=thresh, color='r', linestyle='--')
+    
+# I made this its own function cause I was gonna use it for the confusion matrix
+# Guess we don't need it anymore but I left it
+def get_data(input_list, frames = 180):
+    count = len(input_list)
     data = np.zeros(shape=(count,count))
+    
     for i in range(count):
+        
         for j in range(i, count):
             print("(",i,",",j,")")
+            
             if(i==j):
                 val=0
             else:
@@ -36,23 +74,16 @@ def draw_dendro(input_list, frames=180, labels=None, thresh=None):
             data[i,j] = val
             data[j,i] = val
     
-    
-    dendrogram(data, labels=labels, thresh=thresh)
-    return data
-            
-
-def dendrogram(data, labels=None, thresh=None):
-    plt.figure(figsize=(10, 7))  
-    plt.title("Dendrograms")
     data = ssd.squareform(data)
-    lkg = shc.linkage(data, method='single')
-    dend = shc.dendrogram(lkg, labels=labels, color_threshold=thresh)
-    if(thresh != None):
-        plt.axhline(y=thresh, color='r', linestyle='--')
+    return data
+
+
+    
     
 if __name__ == '__main__':
     inputs = []
     labels = []
+    #predicted_classes = []# old code from confusion matrix days
      
     # pth = "./data/kitty2.graphml"  #0
     # inp = dr.read_graphml(pth)
@@ -76,7 +107,9 @@ if __name__ == '__main__':
     
     # labels = ["kitty2 (1)","kitty1","kitty2 (2)","Brutus","Sofie"]
     
-    z = tud.get_z()
+    p = "data/Letter-low"
+    ds = "Letter-low"
+    z = tud.read_tud(p,ds)
     
     num = 5
     
@@ -95,6 +128,7 @@ if __name__ == '__main__':
         
         inputs.append( (G, pos) )
         labels.append("Z " + str(i))
+        #predicted_classes.append("Z")# old code from confusion matrix days
     
     #Get 5 L's
     for i in range(num):
@@ -104,6 +138,7 @@ if __name__ == '__main__':
         
         inputs.append( (G, pos) )
         labels.append("L " + str(i))
+        #predicted_classes.append("L")# old code from confusion matrix days
     
     #Get 5 N's
     for i in range(num):
@@ -113,6 +148,7 @@ if __name__ == '__main__':
         
         inputs.append( (G, pos) )
         labels.append("N " + str(i))
+        #predicted_classes.append("N")# old code from confusion matrix days
 
         
     #Get 5 V's
@@ -123,6 +159,7 @@ if __name__ == '__main__':
         
         inputs.append( (G, pos) )
         labels.append("V " + str(i))
+        #predicted_classes.append("V")# old code from confusion matrix days
     
     # pth = "./data/Binary Images/cat2.png"  #0
     # inp = dr.read_img(pth)
@@ -152,3 +189,55 @@ if __name__ == '__main__':
     #data = np.array( [[0,1,10,7],[1,0,8,9],[10,8,0,2],[7,9,2,0]] )
     #data = np.array([1,10,8,7,9,2])
     #dendrogram(data, labels=labels)
+    
+    
+###############################################################################
+# Old code from back when we thought we were going to use a confusion matrix
+# Didn't wanna throw it away so here it is.
+# It's not at all near complete but this is where I stopped so ¯\_(ツ)_/¯
+
+# def confusion(input_list, labels, thresh, predicted_classes):
+#     data = get_data(input_list)# Get the data in the format we want
+#     lkg = shc.linkage(data, method='single')# Get the linkage
+    
+#     # Get the list of all the classes that actually appeared in the data
+#     # The index in this corresponds to the elements at the same index in input_list
+#     actual_classes = list(shc.fcluster(lkg, thresh, criterion='distance'))
+    
+#     # Columns in the confusion matrix correspond to the predicted labels
+#     n = len(predicted_classes)
+#     # Rows in the confusion matrix correspond to the actual labels
+#     m = len(actual_classes)
+#     # start with an empty m by n matrix
+#     confusionMatrix = [ [0 for i in range(n)] for j in range(m) ]
+#     pkey = {} # Going to use dictionaries to keep track of the indices of labels
+#     akey = {}
+#     index = 0
+#     known = []
+#     for p in predicted_classes:
+#         pkey[index] = p # Adding the classes to the dictionaries
+#         akey[index] = p
+#         index += 1
+#         known.append(p)
+#     for a in actual_classes:
+#         if a not in known:
+#             known.append(a)
+#             akey[index] = a
+#             index += 1
+    
+#     for i in range(len(labels)):
+#         p = predicted_classes[i] # Predicted class of a certain graph
+#         a = actual_classes[i] # Actual class of a the same graph
+
+
+# # Scipy will group things together, but we need to figure out what the group labels mean
+# def convert_classes(linkage, input_list, predicted_classes, actual_classes):S
+#     # We're going to assume that the most prevalent label in an actual class
+#     # is the proper label for that class
+#     members = {} # Dictionary of lists of class members
+#     for c in range(len(actual_classes)):
+#         a = actual_classes[c]
+#         p = predicted_classes[c]
+#         if a not in members:
+#             members[a] = []
+#         members[a].append(p)
