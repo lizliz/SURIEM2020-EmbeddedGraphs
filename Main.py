456617e0ec018b335550979550f55a9e9a63c092
Candace Todd <54723302+CLTodd@users.noreply.github.com>
2020-07-23 00:40:05 -0400

    changing import names

--- Main.py
+++ Main.py
@@ -6,8 +6,8 @@ Created on Tue Jul 21 07:49:13 2020
 """
 # Run this script to access functions from any script in the repo
 
-import Classify as c
-import Compare as C
+import Classify as class
+import Compare as comp
 import DataCalculations as dc
 import Merge as m
 import Visualization as v
